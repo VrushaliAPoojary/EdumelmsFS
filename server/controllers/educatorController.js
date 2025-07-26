bba62a8a2ebb44f7336354e1cf3da7d2653bdb68
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-27 00:57:05 +0530

    multer,authmiddleware done

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -19,3 +19,25 @@ export const updateRoleToEducator = async (req,res)=>{
 
     }
 }
+
+//ad new course
+export const addNewCourse = async (req,res)=>{
+    try{
+        const {courseData} = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+        if(!imageFile){
+            return res.json({success:false, message: 'Thumbnail not attatched'})
+        }
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+        res.json({success:true, message: 'Course created successfully'})
+    } catch(error){
+        res.json({success:false, message: error.message})
+
+    }
+}
