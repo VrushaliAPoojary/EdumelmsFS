e5793143e013f23a39a09956c884c8b6b88d3893
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-28 00:55:49 +0530

    ..

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -4,18 +4,15 @@ import Navbar from '../../components/educator/Navbar'
 import Sidebar from '../../components/educator/Sidebar'
 import Footer from '../../components/educator/Footer'
 
-
 const Educator = () => {
   return (
-    <div className='text-default min-h-screen bg-white' >
+    <div className='text-default min-h-screen bg-white'>
       <Navbar />
       <div className='flex'>
         <Sidebar />
         <div className='flex-1'>
-         {<outlet/>}
+          <Outlet /> {/* ✅ Correct usage */}
         </div>
-
-
       </div>
       <Footer />
     </div>
