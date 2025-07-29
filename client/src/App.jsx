191c6db23faf5df956d3ad7546305a264a9169d1
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-29 22:57:10 +0530

    front end working perfectly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { Route, Routes, useMatch } from 'react-router-dom'
-import Home from './pages/student/Home'
-import CourseList from './pages/student/CourseList'
-import CourseDetails from './pages/student/CourseDetails'
-import MyEnrollments from './pages/student/MyEnrollments'
-import Player from './pages/student/Player'
-import Loading from './components/student/Loading'
-import Educator from './pages/educator/Educator'
-import AddCourse from './pages/educator/AddCourse'
-import MyCourses from './pages/educator/MyCourses'
-import StudentsEnrolled from './pages/educator/StudentsEnrolled'
-import Dashboard from './pages/educator/Dashboard'
-import Navbar from './components/educator/Navbar'
+import React from 'react';
+import { Route, Routes, useMatch } from 'react-router-dom';
+import Home from './pages/student/Home';
+import CourseList from './pages/student/CourseList';
+import CourseDetails from './pages/student/CourseDetails';
+import MyEnrollments from './pages/student/MyEnrollments';
+import Player from './pages/student/Player';
+import Loading from './components/student/Loading';
+import Educator from './pages/educator/Educator';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
+import Dashboard from './pages/educator/Dashboard';
+import Navbar from './components/student/Navbar'; // Student navbar
 import "quill/dist/quill.snow.css";
 
 const App = () => {
-  const isEducatorRoute = useMatch('/educator/*')
+  const isEducatorRoute = useMatch('/educator/*');
+
   return (
     <div className='text-default min-h-screen bg-white'>
-      {isEducatorRoute && <Navbar />}
+      {/* ✅ Show student Navbar on all non-educator pages */}
+      {!isEducatorRoute && <Navbar />}
+
       <Routes>
         {/* Student Routes */}
         <Route path='/' element={<Home />} />
@@ -29,16 +32,16 @@ const App = () => {
         <Route path='/player/:courseId' element={<Player />} />
         <Route path='/loading/:path' element={<Loading />} />
 
-        {/* Educator Routes with nested children */}
+        {/* Educator Routes — this layout includes its own navbar/sidebar/footer */}
         <Route path='/educator' element={<Educator />}>
-          <Route index element={<Dashboard />} /> {/* Default child at /educator */}
-          <Route path='add-course' element={<AddCourse />} /> {/* /educator/add-course */}
-          <Route path='my-courses' element={<MyCourses />} /> {/* /educator/my-courses */}
-          <Route path='student-enrolled' element={<StudentsEnrolled />} /> {/* /educator/student-enrolled */}
+          <Route index element={<Dashboard />} />
+          <Route path='add-course' element={<AddCourse />} />
+          <Route path='my-courses' element={<MyCourses />} />
+          <Route path='student-enrolled' element={<StudentsEnrolled />} />
         </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
