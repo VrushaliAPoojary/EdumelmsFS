9c89ceae5af326c504b3aa3bbaff1707b1fb6745
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-31 17:06:56 +0530

    api working

---
+++ server/controllers/userController.js
@@ -0,0 +1,34 @@
+import User from "../models/user.js"
+
+
+
+export const getUserData = async (req, res) => {
+    try{
+        const userId = req.auth.userId
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.json({success: false, message: 'User not found'})
+        }
+
+        res.json({success: true, user})
+
+    } catch (error) {
+
+        res.json({success: false, message: error.message})
+    }
+}
+
+// user enrolled courses with lecture links
+export const userEnrolledCourses = async (req, res) => {
+    try{
+        const userId = req.auth.userId
+        const userData = await User.findById(userId).populate('enrolledCourses')
+
+        res.json({success: true, userEnrolledCourses: userData.enrolledCourses})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+
+    }
+}
